b1716d97b8907a69fd2b40abd36daca312968b28
actionanand <meetanandns@gmail.com>
2019-10-20 14:41:02 +0530

    analysing req using tap in interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
 
@@ -6,6 +7,15 @@ export class AuthInterceptorService implements HttpInterceptor {
     console.log('Request is on the way!');
     // const modifiedReq = req.clone({url: 'www.new-url.com'});
     const modifiedReq = req.clone({headers: req.headers.append('Auth','xyz')});
-    return next.handle(modifiedReq);
+    return next.handle(modifiedReq).pipe(
+      tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Response){
+          console.log('Event Arrived'+ event.body);
+          console.log(event.body);
+        }
+      })
+    );
   }
+
 }
